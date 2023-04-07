29d838f965232c571f391e6d614de3fa386ba9ce
ABDUL HAJEES <84639525+aj05hacker@users.noreply.github.com>
2023-04-08 00:09:55 +0530

    Add files via upload

---
+++ aj-admin-panel-finder/ajadminfinder.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from ajadminfinder import ajadminfinder as tool
+
+
+def main():
+    tool.main()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        pass
